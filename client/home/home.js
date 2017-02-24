cd9b28c61e6a5f7088483f3be542cbd8a740656b
Ben Wingerter <benwingerter01@gmail.com>
2017-02-24 12:16:56 -0600

    front end additions'

--- client/home/home.js
+++ client/home/home.js
@@ -1,18 +1,36 @@
 import { Meteor } from 'meteor/meteor';
 import { Tempalate } from 'meteor/templating';
 import { Profiles } from '../../lib/profile.js';
+import { Index, MinimongoEngine } from 'meteor/easy:search';
 
 ProfilesIndex = new EasySearch.Index({
     collection: Profiles,
     fields: ['name'],
-    engine: new EasySearch.MongoDB()
+    engine: new MinimongoEngine({
+        /*selector: (searchObject, options, aggregation) => {
+
+            let selector = this.defaultConfiguration().selector(searchObject, options, aggregation);
+
+            if(selector.name === "") {
+                searchObject.name = "dsfadfasdfadsfadsfasdfdsf";
+            }
+
+            return selector;
+        }*/
+    })
+});
+
+Template.home.onCreated(function() {
+    Meteor.subscribe("profiles");
 });
 
 Template.home.helpers({
-    profilesIndex: () => ProfilesIndex,
+    profilesIndex: () => {
+        return ProfilesIndex;
+    },
     inputAttributes: () => {
         return {
-            placeholder: 'Search for a game or player',
+            placeholder: 'Search for a user',
             style: 'width: 500px; height: 30px; padding: 5px; display: block;'
         };
     }
